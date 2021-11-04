32028719a185ada11c0d9477f17661e7138ade0f
Tiffany Patek <tdsloan@gmail.com>
2021-11-04 13:56:35 -0600

    Moving to new repo

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS volunteers_db;
+CREATE DATABASE volunteers_db;
+
+CREATE TABLE IF NOT EXISTS users;
+INSERT INTO users
+VALUES ("Turd", "Ferguson", "tferg@mess.com", "uncle", 3, 1940-07-04, 400);
+
+CREATE TABLE IF NOT EXISTSgit st

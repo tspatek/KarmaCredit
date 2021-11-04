7f73d46bb201e3f9e2e49b35e35652bba777a13b
Tiffany Patek <tdsloan@gmail.com>
2021-11-04 14:56:27 -0600

    Formatted

--- public/js/event-signup.js
+++ public/js/event-signup.js
@@ -1,11 +1,3 @@
-
-//[ ] GET from db the info of the selected event
-//store them in variables
-
-//[ ] jquery inject them into the page
-
-//[ ] POST ajax request for putting information into event's & user's db entry
-
 const apiPostRoute;
 $.post(apiPostRoute, me, res => {
     console.log(res);

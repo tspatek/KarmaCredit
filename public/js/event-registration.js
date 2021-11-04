97d68a56b5063e10db97bd99b538491b2541ab9d
Tiffany Patek <tdsloan@gmail.com>
2021-11-04 14:53:27 -0600

    Update event-registration.js

--- public/js/event-registration.js
+++ public/js/event-registration.js
@@ -2,15 +2,6 @@ $("body").click(()=>{
     console.log('event registration page works.')
 });
 
-
 // $('#submit-btn').click(()=>{
-//     event.preventDefault();
-//     console.log('submit was clicked, but...');
-//     //first, we need to validate entry information
-
-//     //then we need to POST all that information-->
-//     //$$$$$$$$$$$$POST req.body
-//     //so the event's database entry can be created.
-
-    
+//     event.preventDefault();    
 // });

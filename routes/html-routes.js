de2d9cdf0723945a6c70ae3c3002d9a03880f03c
Tiffany Patek <tdsloan@gmail.com>
2021-11-04 14:35:48 -0600

    Formatted

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,7 @@
-// Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-
-// Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
-
     let userLoggedIn;
     app.get("/", function (req, res) {
         // If the user already has an account send them to the members page
@@ -18,25 +14,21 @@ module.exports = function (app) {
         // else {
         //     userLoggedIn = false;
         //     res.json([req.user, userLoggedIn]);
-
         // }
         res.sendFile(path.join(__dirname, "../public/events.html"));
     });
 
     app.get("/login", function (req, res) {
-        // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/");
         }
         res.sendFile(path.join(__dirname, "../public/login-2.html"));
     });
 
-    // Here we've add our isAuthenticated middleware to this route.
-    // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/byeee", isAuthenticated, function (req, res) {
         res.sendFile(path.join(__dirname, "../public/byeee.html"));
     });
-    //This is a temporary html route
+    
     app.get("/event-mgmt", isAuthenticated, function (req, res) {
         res.sendFile(path.join(__dirname, "../public/event-management.html"));
     });
